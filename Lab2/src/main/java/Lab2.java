03f80fe367860710e7cffa6a716caa879ca7e314
JenyaFTW <yyakovliev02@gmail.com>
2022-12-13 14:18:35 +0200

    feat: add lab 2

--- Lab2/src/main/java/Lab2.java
+++ Lab2/src/main/java/Lab2.java
@@ -1,5 +1,55 @@
 public class Lab2 {
+    public static void displayMatrix(byte[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.printf("%d ", matrix[i][j]);
+                if (j == matrix[i].length - 1) {
+                    System.out.print("\n");
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*
+
+        Zalikova: 0526
+        C5 = 1 (C=Bт)
+        C7 = 1 (byte)
+        C11 = 9 (Знайти середнє значення елементів кожного стовпчика матриця)
+
+         */
+
+        byte matrixB[][] = {
+                { 6, 5, 4, 3, 2 },
+                { 12, 6, 3, 2, 1 },
+                { 3, 5, -2, 4, 9 }
+        };
+
+        System.out.println("Initial matrix:");
+        Lab2.displayMatrix(matrixB);
+
+        // First step: Transpose matrix
+        int rows = matrixB.length;
+        int cols = matrixB[0].length;
+        byte matrixC[][] = new byte[cols][rows];
+
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                matrixC[i][j] = matrixB[j][i];
+            }
+        }
+
+        System.out.println("\nMatrix after transposing:");
+        Lab2.displayMatrix(matrixC);
+
+        System.out.println("\nAverages of each column:");
+        for (int i = 0; i < matrixC[0].length; i++) {
+            byte total = 0;
+            for (int j = 0; j < matrixC.length; j++) {
+                total += matrixC[j][i];
+            }
+            System.out.printf("Col = %d | Avg = %d\n", i, total / matrixC.length);
+        }
     }
 }
